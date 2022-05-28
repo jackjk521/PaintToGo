0c7e3c9399bbe090e713a93dfcfb408a06aa7cab
jackjk521 <jedchu541@gmail.com>
2022-05-28 19:22:03 +0800

    rested to base here

---
+++ PaintToGo/resources/js/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default function Navbar() {
+
+    return (
+        <div>
+            <nav class="navbar fixed-top navbar-dark bg-dark">
+                <a class="navbar-brand px-2" href="dashboard">
+                    Color City Paint Store
+                </a>
+            </nav>
+        </div>
+    );
+}
+
